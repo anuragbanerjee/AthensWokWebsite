8dd2e4076a56c389b49faf1a8e112df5e456c91e
Anurag Banerjee <anuragb2010@gmail.com>
2016-03-22 11:21:20 -0400

    Initial commit, MVP site

---
+++ assets/scripts.js
@@ -0,0 +1,250 @@
+var menu = [
+  {
+    "category": "Appetizers",
+    "entries":[
+      {"name": "A01 Calamari", "price": "5.95"},
+      {"name": "A02 Fried Tofu", "price": "3.95"},
+      {"name": "A03 Edamame", "price": "3.95"},
+      {"name": "A04 House Salad", "price": "3.95"},
+      {"name": "A05 Seaweed Salad", "price": "3.95"},
+      {"name": "A06 Satay Chicken (3)", "price": "4.95"},
+      {"name": "A07 Fried Wonton (6 pieces)", "price": "2.95"},
+      {"name": "A08 Fried Chicken Wings (4)", "price": "3.95"},
+      {"name": "A09 Crab Rangoon (4 pieces)", "price": "3.95"},
+      {"name": "A10 Fried Spring Roll (2 Rolls)", "price": "2.45"},
+      {"name": "A11 Tempura Shrimp & Veggie", "price": "5.95"},
+      {"name": "A12 Fried Egg Roll (2 Roll) Beef", "price": "2.45"},
+      {"name": "A13 Potsticker Pork (4 pieces)", "price": "3.95"},
+      {"name": "A13 Gyoza Pork (4 pieces)", "price": "3.95"},
+      {"name": "A14 Fresh Basil Roll Shrimp (2 Roll)", "price": "3.95"},
+      {"name": "A14 Fresh Basil Roll Tofu (2 Roll)", "price": "3.95"}
+    ]
+  },
+
+  {
+    "category": "Soups",
+    "entries": [
+      {"name": "S01 Wonton Soup", "price": "1.59+"},
+      {"name": "S01 Hot & Sour Soup", "price": "1.59+"},
+      {"name": "S03 Tom Yum Shrimp Soup", "price": "2.99+"},
+      {"name": "S03 Coconut Shrimp Soup", "price": "2.99+"},
+      {"name": "S05 Tom Yum Chicken Soup", "price": "1.99+"},
+      {"name": "S05 Coconut Chicken Soup", "price": "1.99+"},
+      {"name": "S07 Miso Soup", "price": "1.95+"},
+      {"name": "S07 Egg Drop Soup", "price": "1.95+"}
+    ]
+  },
+
+  {
+    "category": "Desserts",
+    "entries":[
+      {"name": "D02 Fried Banana", "price": "4.95"},
+      {"name": "D05 Curry Pop Pie Apple", "price": "2.95"},
+      {"name": "D06 Cheese Cake", "price": "4.95"},
+      {"name": "D06 Chocolate Cake", "price": "4.95"},
+    ]
+  },
+
+  {
+    "category": "Chinese BBQ",
+    "entries": [
+      {"name": "B01 Pork Rib (Dinner)", "price": "8.95"},
+      {"name": "B02 BBQ Pork (Dinner)", "price": "8.95"},
+      {"name": "B03 Roast Beef (Dinner)", "price": "8.95"},
+      {"name": "B04 Roast Pork (Dinner)", "price": "8.95"},
+      {"name": "B06 Soy Sauce Chicken (Dinner)", "price": "8.95"},
+      {"name": "BBQ Duck (Dinner)", "price": "10.95"}
+    ]
+  },
+
+  {
+    "category": "Noodle Soup",
+    "entries": [
+      {"name": "N01 Pho Beef Soup", "price": "7.95"},
+      {"name": "N02 Chicken in Yellow Noodle Soup", "price": "7.95"},
+      {"name": "N02 Pork in Yellow Noodle Soup", "price": "7.95"},
+      {"name": "N03 Chicken in Wonton & Yellow Noodle Soup", "price": "7.95"},
+      {"name": "N03 Pork in Wonton & Yellow Noodle Soup", "price": "7.95"}
+    ]
+  },
+
+  {
+    "category": "Hibachi",
+    "entries": [
+      {"name": "L91 Hibachi Veggie (Dinner)", "price": "10.95"},
+      {"name": "L92 Hibachi Shrimp (Dinner)", "price": "14.95"},
+      {"name": "L93 Hibachi Salmon (Dinner)", "price": "14.95"},
+      {"name": "L94 Hibachi Teriyaki Steak (Dinner)", "price": "14.95"},
+      {"name": "L95 Hibachi New York Strip (Dinner)", "price": "14.95"},
+      {"name": "L96 Hibachi Teriyaki Chicken (Dinner)", "price": "12.95"},
+      {"name": "L97 Hibachi Fried Rice (Dinner)", "price": "9.95"},
+      {"name": "L97 Hibachi Beef Fried Rice (Dinner)", "price": "11.95"},
+      {"name": "L97 Hibachi Chicken Fried Rice (Dinner)", "price": "11.95"},
+      {"name": "L97 Hibachi BBQ Pork Fried Rice (Dinner)", "price": "11.95"},
+      {"name": "L97 Hibachi Shrimp Fried Rice (Dinner)", "price": "11.95"}
+    ]
+  },
+
+  {
+    "category": "Chinese Dinner Entrees",
+    "entries": [
+      {"name": "C01 Chicken Fried Rice (Dinner)", "price": "8.95"},
+      {"name": "C01 Pork Fried Rice (Dinner)", "price": "8.95"},
+      {"name": "C01 Beef Fried Rice (Dinner)", "price": "8.95"},
+      {"name": "C01 House Fried Rice (Dinner)", "price": "8.95"},
+      {"name": "C01 Shrimp Fried Rice (Dinner)", "price": "10.95"},
+      {"name": "C02 Moo Goo Gai Pan (Dinner)", "price": "8.95"},
+      {"name": "C03 Teriyaki Chicken (Dinner)", "price": "8.95"},
+      {"name": "C03 Teriyaki Beef (Dinner)", "price": "8.95"},
+      {"name": "C04 Shrimp with Lobster Sauce (Dinner)", "price": "10.95"},
+      {"name": "C05 Spicy Mafu tofu (Dinner)", "price": "8.95"},
+      {"name": "C06 Kung Pao Chicken (Dinner)", "price": "8.95"},
+      {"name": "C06 Kung Pao Beef (Dinner)", "price": "8.95"},
+      {"name": "C07 House Special Noodle with Chicken (Dinner)", "price": "8.95"},
+      {"name": "C07 House Special Noodle with Beef (Dinner)", "price": "8.95"},
+      {"name": "C08 Orange Chicken (Dinner)", "price": "8.95"},
+      {"name": "C09 Pepper Steak (Dinner)", "price": "8.95"},
+      {"name": "C09 Pepper Chicken (Dinner)", "price": "8.95"},
+      {"name": "C10 Beef & Broccoli (Dinner)", "price": "8.95"},
+      {"name": "C10 Chicken & Broccoli (Dinner)", "price": "8.95"},
+      {"name": "C11 General Tao’s Chicken (Dinner)", "price": "8.95"},
+      {"name": "C12 Lomein (Dinner)", "price": "8.95"},
+      {"name": "C13 Mongolian Beef (Dinner)", "price": "8.95"},
+      {"name": "C13 Mongolian Chicken (Dinner)", "price": "8.95"},
+      {"name": "C14 Sesame Chicken (Dinner)", "price": "8.95"},
+      {"name": "C15 Garlic Chicken (Dinner)", "price": "8.95"},
+      {"name": "C15 Garlic Beef (Dinner)", "price": "8.95"},
+      {"name": "C16 Sweet & Sour Chicken (Dinner)", "price": "8.95"},
+      {"name": "C16 Sweet & Sour Shrimp (Dinner)", "price": "10.95"},
+      {"name": "C17 Cashew Nut Chicken (Dinner)", "price": "8.95"},
+      {"name": "C17 Cashew Nut Beef (Dinner)", "price": "8.95"}
+    ]
+  },
+
+  {
+    "category": "Thai Dinner Entrees",
+    "entries": [
+      {"name": "T01 Thai Fried Rice (Dinner)", "price": "8.95"},
+      {"name": "T02 Pad See U (Dinner)", "price": "8.95"},
+      {"name": "T03 Pad Basil (Dinner)", "price": "8.95"},
+      {"name": "T04 Pad Pik Pow (Dinner)", "price": "8.95"},
+      {"name": "T05 Pad Pik Khing (Dinner)", "price": "8.95"},
+      {"name": "T06 Basil Fried Rice (Dinner)", "price": "8.95"},
+      {"name": "T08 Panang Curry (Dinner)", "price": "8.95"},
+      {"name": "T08 Green Curry (Dinner)", "price": "8.95"},
+      {"name": "T08 Yellow Curry (Dinner)", "price": "8.95"},
+      {"name": "T08 Masaman Curry (Dinner)", "price": "8.95"},
+      {"name": "T10 Pad Thai (Dinner)", "price": "8.95"},
+      {"name": "T11 Larb Chicken (Dinner)", "price": "8.95"},
+      {"name": "T11 Larb Pork (Dinner)", "price": "8.95"},
+      {"name": "T11 Larb Beef (Dinner)", "price": "8.95"},
+      {"name": "T12 Pad Kee Mao (Dinner)", "price": "8.95"}
+    ]
+  },
+
+  {
+    "category": "Chef's Special",
+    "entries": [
+      {"name": "SF01 Spicy Garlic", "price": "11.59"},
+      {"name": "SF02 Hot Pot Fish", "price": "12.95"},
+      {"name": "SF02 Hot Pot House", "price": "11.59"},
+      {"name": "SF02 Hot Pot Seafood", "price": "13.59"},
+      {"name": "SF03 Pad Basil", "price": "11.59"},
+      {"name": "SF04 Pan Fried Noodle Seafood", "price": "13.59"},
+      {"name": "SF04 Pan Fried Noodle House", "price": "11.59"},
+      {"name": "SF05 Lomein Seafood", "price": "13.59"},
+      {"name": "SF05 Lomein House", "price": "11.59"},
+      {"name": "SF06 Panang Curry", "price": "11.59"},
+      {"name": "SF06 Green Curry", "price": "11.59"},
+      {"name": "SF06 Yellow Curry", "price": "11.59"},
+      {"name": "SF06 Masaman Curry", "price": "11.59"}
+    ]
+  },
+
+  {
+    "category": "Hibachi Lunch Menu",
+    "entries": [
+      {"name": "L91 Hibachi Veggie (Lunch)", "price": "6.95"},
+      {"name": "L92 Hibachi Shrimp (Lunch)", "price": "7.95"},
+      {"name": "L93 Hibachi Salmon (Lunch)", "price": "7.95"},
+      {"name": "L94 Hibachi Teriyaki Steak (Lunch)", "price": "7.95"},
+      {"name": "L95 Hibachi New York Strip (Lunch)", "price": "7.95"},
+      {"name": "L96 Hibachi Teriyaki Chicken (Lunch)", "price": "6.95"},
+      {"name": "L97 Hibachi Fried Rice (Lunch)", "price": "6.95"}
+    ]
+  },
+
+  {
+    "category": "Chinese Lunch Menu",
+    "entries": [
+      {"name": "C01 Chicken Fried Rice (Lunch)", "price": "6.95"},
+      {"name": "C01 Pork Fried Rice (Lunch)", "price": "6.95"},
+      {"name": "C01 Beef Fried Rice (Lunch)", "price": "6.95"},
+      {"name": "C01 House Fried Rice (Lunch)", "price": "6.95"},
+      {"name": "C01 Shrimp Fried Rice (Lunch)", "price": "7.95"},
+      {"name": "C02 Moo Goo Gai Pan (Lunch)", "price": "6.95"},
+      {"name": "C03 Teriyaki Chicken (Lunch)", "price": "6.95"},
+      {"name": "C03 Teriyaki Beef (Lunch)", "price": "6.95"},
+      {"name": "C04 Shrimp with Lobster Sauce (Lunch)", "price": "7.95"},
+      {"name": "C05 Spicy Mafu tofu (Lunch)", "price": "6.95"},
+      {"name": "C06 Kung Pao Chicken (Lunch)", "price": "6.95"},
+      {"name": "C06 Kung Pao Beef (Lunch)", "price": "6.95"},
+      {"name": "C07 House Special Noodle with Chicken (Lunch)", "price": "6.95"},
+      {"name": "C07 House Special Noodle with Beef (Lunch)", "price": "6.95"},
+      {"name": "C08 Orange Chicken (Lunch)", "price": "6.95"},
+      {"name": "C09 Pepper Steak (Lunch)", "price": "6.95"},
+      {"name": "C09 Pepper Chicken (Lunch)", "price": "6.95"},
+      {"name": "C10 Beef & Broccoli (Lunch)", "price": "6.95"},
+      {"name": "C10 Chicken & Broccoli (Lunch)", "price": "6.95"},
+      {"name": "C11 General Tao’s Chicken (Lunch)", "price": "6.95"},
+      {"name": "C12 Lomein (Lunch)", "price": "6.95"},
+      {"name": "C13 Mongolian Beef (Lunch)", "price": "6.95"},
+      {"name": "C13 Mongolian Chicken (Lunch)", "price": "6.95"},
+      {"name": "C14 Sesame Chicken (Lunch)", "price": "6.95"},
+      {"name": "C15 Garlic Chicken (Lunch)", "price": "6.95"},
+      {"name": "C15 Garlic Beef (Lunch)", "price": "6.95"},
+      {"name": "C16 Sweet & Sour Chicken (Lunch)", "price": "6.95"},
+      {"name": "C16 Sweet & Sour Shrimp (Lunch)", "price": "7.95"},
+      {"name": "C17 Cashew Nut Chicken (Lunch)", "price": "6.95"},
+      {"name": "C17 Cashew Nut Beef (Lunch)", "price": "6.95"}
+    ]
+  },
+
+  {
+    "category": "Chinese BBQ Lunch Menu",
+    "entries": [
+      {"name": "B01 Pork Rib (Lunch)", "price": "6.95"},
+      {"name": "B02 BBQ Pork (Lunch)", "price": "6.95"},
+      {"name": "B03 Roast Beef (Lunch)", "price": "6.95"},
+      {"name": "B04 Roast Pork (Lunch)", "price": "6.95"},
+      {"name": "B06 Soy Sauce Chicken (Lunch)", "price": "6.95"},
+      {"name": "BBQ Duck (Lunch)", "price": "7.95"}
+    ]
+  },
+
+  {
+    "category": "Thai Lunch Menu",
+    "entries": [
+      {"name": "T01 Thai Fried Rice (Lunch)", "price": "6.95"},
+      {"name": "T02 Pad See U (Lunch)", "price": "6.95"},
+      {"name": "T03 Pad Basil (Lunch)", "price": "6.95"},
+      {"name": "T04 Pad Pik Pow (Lunch)", "price": "6.95"},
+      {"name": "T05 Pad Pik Khing (Lunch)", "price": "6.95"},
+      {"name": "T06 Basil Fried Rice (Lunch)", "price": "6.95"},
+      {"name": "T08 Panang Curry (Lunch)", "price": "6.95"},
+      {"name": "T08 Green Curry (Lunch)", "price": "6.95"},
+      {"name": "T08 Yellow Curry (Lunch)", "price": "6.95"},
+      {"name": "T08 Masaman Curry (Lunch)", "price": "6.95"},
+      {"name": "T10 Pad Thai (Lunch)", "price": "6.95"},
+      {"name": "T11 Larb Chicken (Lunch)", "price": "6.95"},
+      {"name": "T11 Larb Pork (Lunch)", "price": "6.95"},
+      {"name": "T11 Larb Beef (Lunch)", "price": "6.95"},
+      {"name": "T12 Pad Kee Mao (Lunch)", "price": "6.95"}
+    ]
+  }
+];
+
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+var result = template({"menu": menu});
+$("#menu").append(result);
